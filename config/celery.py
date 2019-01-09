e5e9713caeeffb8c4a28abed809f5cc82086f361
AverHLV <moc9.cs4ever@gmail.com>
2019-01-09 17:39:16 +0200

    Multiple settings added

--- config/celery.py
+++ config/celery.py
@@ -1,8 +1,8 @@
-from config.settings.base import BASE_NAME
+from config.settings.local import BASE_NAME
 from celery import Celery
 from os import environ
 
-environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.base')
+environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
 app = Celery(BASE_NAME)
 app.config_from_object('django.conf:settings', namespace='CELERY')
--- config/wsgi.py
+++ config/wsgi.py
@@ -7,10 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
+# import os
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
 
 application = get_wsgi_application()
