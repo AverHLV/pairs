201cecdf27e1e3e97d5be0195c922bba0fe26a1a
AverHLV <moc9.cs4ever@gmail.com>
2019-08-29 14:39:42 +0300

    Finder update

--- config/constants.py
+++ config/constants.py
@@ -210,4 +210,21 @@ old_stats_days_live = 5
 # finder interface
 timeout = 5
 threshold_month_number = 3
+title_n_words = 8
 rank_drop = 1000
+
+stopwords = [
+    'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he',
+    'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs',
+    'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were',
+    'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but',
+    'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between',
+    'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on',
+    'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all',
+    'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same',
+    'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'pcs', 'tool', 'best',
+    'premium', 'pack', 'easy', 'safe', 'superior', 'exclusive'
+]
+
+stopwords += ['set of {}'.format(i) for i in range(10)]
+stopwords += ['{} pack'.format(i) for i in range(10)]
